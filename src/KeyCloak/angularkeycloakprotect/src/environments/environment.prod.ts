94624dcfd5920c4c6839f19f146ce23681199d4e
Paulo Eduardo Correia <correia97@gmail.com>
2020-12-20 09:26:11 -0300

    projeto angular no com docker

--- src/KeyCloak/angularkeycloakprotect/src/environments/environment.prod.ts
+++ src/KeyCloak/angularkeycloakprotect/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  authBaseUrl : 'http://192.168.0.5:8088',
+  clientAuth: 'exemplo1',
+  apiBaseUrl: 'http://192.168.0.5:8081'
 };
