90cffef78b3ee507874d9350f2a6516965543685
Paulo Eduardo Correia <32842658+pecorreia@users.noreply.github.com>
2022-08-11 11:03:12 -0300

    geração e configuração correta do certificado auto assinado para localhost

--- src/Cadastro.MVC/Program.cs
+++ src/Cadastro.MVC/Program.cs
@@ -77,7 +77,8 @@ if (app.Environment.EnvironmentName.ToUpper().Contains("PROD"))
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
+    //app.UseHttpsRedirection();
 }
 else
 {
@@ -85,7 +86,6 @@ else
     IdentityModelEventSource.ShowPII = true;
 }
 
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
